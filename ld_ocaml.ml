f8e4a8130118a0826b59a8c02ea8f92d0bfc30a5
Mauricio Fernandez <mfp@acm.org>
2009-12-01 19:56:23 +0100

    ld_ocaml: use known module table, handle multiple cmxs and show solution.

--- ld_ocaml.ml
+++ ld_ocaml.ml
@@ -1,6 +1,7 @@
 
 open Printf
 open Ld_util
+open Ld_known_modules
 
 let catalog = build_catalog ()
 
@@ -15,3 +16,19 @@ let () =
     prerr_endline (String.make 78 '=');
   end
 
+let () =
+  let state = state_of_known_modules ~known_interfaces ~known_implementations in
+  let rec extract_cmxs_args = function
+    | f :: tl when is_cmxs f -> f :: extract_cmxs_args tl
+    | _ -> [] in
+  let cmxs = extract_cmxs_args (List.tl (Array.to_list (Sys.argv))) in
+    (* TODO: should rewrite Sys.argv instead, need to extract cmxs args in C
+     * code before initializing the caml runtime *)
+    Arg.current := 1 + List.length cmxs;
+    let sol = resolve catalog state cmxs in
+      if !debug then begin
+        eprintf "Loading:\n";
+        List.iter (eprintf "  %s\n") cmxs;
+        eprintf "Will load these libraries:\n";
+        List.iter (fun lib -> eprintf "  %s\n" lib.lib_filename) sol.st_libs
+      end
